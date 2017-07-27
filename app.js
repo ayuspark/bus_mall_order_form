fc0aeec80450a9770011e69d4ecdc9ed72db2195
philipwerner <philip.r.werner@gmail.com>
2017-07-27 13:38:26 -0700

    constructor for orders going

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+'use strict';
 
+var myOrders = [];
+
+var Order = function(productList, qty, name, address, city, state, zipcode, phone, creditCard){
+  this.productList =
+  this.qty = qty;
+  this.Name = name;
+  this.address = address;
+  this.city = city;
+  this.state = state;
+  this.zipcode = zipcode;
+  this.phone = phone;
+  this.creditCard = creditCard;
+  this.order = [];
+  // this.calcOrderTotal = function(){
+  //   for(var i = 0; i < myOrders.length; i++){
+  //     this.order.push(Math.ceil(this.quantity[i] * 2));
+  //     this.orderTotal += this.order[i];
+  //   }
+  // };
+  this.orderTotal = 0;
+  myOrders.push(this);
+};
