8e5d2e17930b7aa1685152d3d227085032963238
ayuspark <lola.luyaowang@gmail.com>
2017-07-27 14:01:10 -0700

    finished product constructor

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var myOrders = [];
 
 var Order = function(productList, qty, name, address, city, state, zipcode, phone, creditCard){
-  this.productList =
+  this.productList = productList;
   this.qty = qty;
   this.Name = name;
   this.address = address;
@@ -22,3 +22,26 @@ var Order = function(productList, qty, name, address, city, state, zipcode, phon
   this.orderTotal = 0;
   myOrders.push(this);
 };
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//PRODUCT CONSTRUCTOR
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+function Product(name) {
+  this.name = name;
+  this.source = 'img/' + this.name + '.jpg';
+  this.qty = 0;
+  Product.all.push(this);
+}
+
+Product.all = [];
+Product.allNames = ['bag', 'banana', 'bathroom', 'boots', 'breakfast', 'bubblegum', 'chair', 'cthulhu', 'dog-duck', 'dragon', 'pen', 'pet-sweep', 'scissors', 'shark', 'sweep', 'tauntaun', 'unicorn', 'usb', 'water-can', 'wine-glass'];
+Product.allQty = [];
+
+for(var i = 0; i < Product.allNames.length; i++){
+  new Product(Product.allNames[i]);
+} //MAKING 20 INSTANCES
+
+for(var n = 0; n < Product.allNames.length; n++){
+  var optionEl = document.createElement('option');
+  optionEl.textContent = Product.allNames[n];
+  document.getElementById('productList').appendChild(optionEl);
+}//POPULATE DROPDOWN PRODUCT LIST
